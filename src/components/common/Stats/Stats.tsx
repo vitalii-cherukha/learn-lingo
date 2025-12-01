5c7894cc1d5513b4cd5cb574fec229fad333c321
vitalii_cherukha <vitalii.cherukha@gmail.com>
2025-12-01 19:03:14 +0200

    add stats component

--- src/components/common/Stats/Stats.tsx
+++ src/components/common/Stats/Stats.tsx
@@ -1,5 +1,28 @@
+import css from "./Stats.module.css";
+
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <div className={css.statsWrapper}>
+      <ul className={css.list}>
+        <li className={css.item}>
+          <p className={css.itemTitle}>32,000 +</p>
+          <p className={css.itemText}>Experienced tutors</p>
+        </li>
+        <li className={css.item}>
+          <p className={css.itemTitle}>300,000 +</p>
+          <p className={css.itemText}>5-star tutor reviews</p>
+        </li>
+        <li className={css.item}>
+          <p className={css.itemTitle}>120 +</p>
+          <p className={css.itemText}>Subjects taught</p>
+        </li>
+        <li className={css.item}>
+          <p className={css.itemTitle}>200 +</p>
+          <p className={css.itemText}>Tutor nationalities</p>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Stats;
