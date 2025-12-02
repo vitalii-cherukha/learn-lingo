87c9689b6488c983b190eca8026dfdcd11ae4867
vitalii_cherukha <vitalii.cherukha@gmail.com>
2025-12-02 14:23:06 +0200

    add burger menu markup and styles/  fix sprite.svg

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -20,7 +20,7 @@ const Header = () => {
     <Container>
       <header className={css.header}>
         <Link to={"/"} className={css.logoWrapper}>
-          <svg className={css.logoIcon} width="32" height="32">
+          <svg className={css.logoIcon} width="28" height="28">
             <use href="/icon.svg" />
           </svg>
           <span className={css.logoText}>LearnLingo</span>
@@ -41,12 +41,21 @@ const Header = () => {
           </ul>
         </nav>
         <div className={css.action}>
-          <button className={css.actionBtnLogin} type="button">
-            <svg className={css.actionBtnLoginIcon} width="20" height="20">
-              <use href="/sprite.svg#icon-login" />
-            </svg>
-            Log in
-          </button>
+          {isAuthorized ? (
+            <button className={css.actionBtnLogin} type="button">
+              <svg className={css.actionBtnLoginIcon} width="20" height="20">
+                <use href="/sprite.svg#icon-logout" />
+              </svg>
+              Log out
+            </button>
+          ) : (
+            <button className={css.actionBtnLogin} type="button">
+              <svg className={css.actionBtnLoginIcon} width="20" height="20">
+                <use href="/sprite.svg#icon-login" />
+              </svg>
+              Log in
+            </button>
+          )}
           <button className={css.actionBtnRegister} type="button">
             Registration
           </button>
