3c66a88ab9c893a590ec48fcda5cdcf3361cae9f
vitalii_cherukha <vitalii.cherukha@gmail.com>
2025-12-02 11:59:35 +0200

    add favorite route and notFound route/component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router";
 import Header from "./components/layout/Header/Header";
 import Home from "./components/pages/Home/Home";
 import Teachers from "./components/pages/Teachers/Teachers";
+import NotFound from "./components/pages/NotFound/NotFound";
+import Favorites from "./components/pages/Favorites/Favorites";
 
 function App() {
   return (
@@ -11,6 +13,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/teachers" element={<Teachers />} />
+          <Route path="/favorite" element={<Favorites />} />
+          <Route path="/not-found" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
     </>
