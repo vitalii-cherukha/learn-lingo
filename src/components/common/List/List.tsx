0747a3c6e924f9b7318b3b8fd211e20172ea481a
vitalii_cherukha <vitalii.cherukha@gmail.com>
2025-12-11 15:50:51 +0200

    add loader in list component

--- src/components/common/List/List.tsx
+++ src/components/common/List/List.tsx
@@ -2,42 +2,60 @@ import { useEffect, useState } from "react";
 import { Teacher } from "../../../types";
 import { getAllTeachers } from "../../../firebase/database";
 import css from "./List.module.css";
+import Loader from "../Loader/Loader";
+import PageTransition from "../PageTransition/PageTransition";
 
 const List = () => {
   const [teachers, setTeachers] = useState<Teacher[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchTeachers = async () => {
-      const data = await getAllTeachers();
-      setTeachers(data);
+      try {
+        setLoading(true);
+        const data = await getAllTeachers();
+        setTeachers(data);
+      } catch (error) {
+        console.error("Error fetching teachers:", error);
+      } finally {
+        setLoading(false);
+      }
     };
+
     fetchTeachers();
   }, []);
 
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
-    <div className={css.listContainer}>
-      <ul className={css.list}>
-        {teachers.map((teacher) => (
-          <li key={teacher.id} className={css.listItem}>
-            <div className={css.imgWrapper}>
-              <img
-                width="96"
-                height="96"
-                src={teacher.avatar_url}
-                alt={teacher.name}
-                className={css.avatar}
-              />
-            </div>
-            <div className={css.teacherInfo}>
-              <h3>{`${teacher.name} ${teacher.surname}`}</h3>
-              {/* <p>{teacher.lesson_info}</p> */}
-              {/* <p>{teacher.experience}</p> */}
-              <p>{teacher.price_per_hour} €/h</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <PageTransition>
+      {" "}
+      <div className={css.listContainer}>
+        <ul className={css.list}>
+          {teachers.map((teacher) => (
+            <li key={teacher.id} className={css.listItem}>
+              <div className={css.imgWrapper}>
+                <img
+                  width="96"
+                  height="96"
+                  src={teacher.avatar_url}
+                  alt={teacher.name}
+                  className={css.avatar}
+                />
+              </div>
+              <div className={css.teacherInfo}>
+                <h3>{`${teacher.name} ${teacher.surname}`}</h3>
+                {/* <p>{teacher.lesson_info}</p> */}
+                {/* <p>{teacher.experience}</p> */}
+                <p>{teacher.price_per_hour} €/h</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </PageTransition>
   );
 };
 
--- src/components/common/Loader/Loader.tsx
+++ src/components/common/Loader/Loader.tsx
@@ -1,5 +1,12 @@
+import { BounceLoader } from "react-spinners";
+import css from "./Loader.module.css";
+
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div className={css.backdrop}>
+      <BounceLoader color="#121417" size={150} />
+    </div>
+  );
 };
 
 export default Loader;
