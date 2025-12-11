662941a1751e479d2c3891ce4e8b52f594ec2c7b
vitalii_cherukha <vitalii.cherukha@gmail.com>
2025-12-11 18:33:19 +0200

    add markup in list component

--- src/components/common/List/List.tsx
+++ src/components/common/List/List.tsx
@@ -46,10 +46,40 @@ const List = () => {
                 />
               </div>
               <div className={css.teacherInfo}>
-                <h3>{`${teacher.name} ${teacher.surname}`}</h3>
+                <div className={css.titleWrapper}>
+                  <p className={css.titleText}>Languages</p>
+                  <h3
+                    className={css.title}
+                  >{`${teacher.name} ${teacher.surname}`}</h3>
+                </div>
+                <div className={css.infoWrapper}>
+                  <ul className={css.infoList}>
+                    <li className={css.infoListItem}>
+                      <svg width="16" height="16" className={css.infoIcon}>
+                        <use href="/sprite.svg#icon-book" />
+                      </svg>
+                      <p>Lessons online</p>
+                    </li>
+                    <li className={css.infoListItem}>
+                      Lessons done: {teacher.lessons_done}
+                    </li>
+                    <li className={css.infoListItem}>
+                      <svg className={css.infoIconStar} width="16" height="16">
+                        <use href="/sprite.svg#icon-star" />
+                      </svg>
+                      Rating: {teacher.rating}
+                    </li>
+                    <li className={css.infoListItem}>
+                      Price / 1 hour:
+                      <span className={css.price}>
+                        {teacher.price_per_hour}$
+                      </span>
+                    </li>
+                  </ul>
+                </div>
                 {/* <p>{teacher.lesson_info}</p> */}
                 {/* <p>{teacher.experience}</p> */}
-                <p>{teacher.price_per_hour} €/h</p>
+                {/* <p>{teacher.price_per_hour} €/h</p> */}
               </div>
             </li>
           ))}
