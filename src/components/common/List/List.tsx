56e1194fd18e258a7e8cdc5e092b5b5f82fde779
vitalii_cherukha <vitalii.cherukha@gmail.com>
2025-12-12 17:16:34 +0200

    add markup and styles in list component

--- src/components/common/List/List.tsx
+++ src/components/common/List/List.tsx
@@ -31,7 +31,6 @@ const List = () => {
 
   return (
     <PageTransition>
-      {" "}
       <div className={css.listContainer}>
         <ul className={css.list}>
           {teachers.map((teacher) => (
@@ -46,40 +45,65 @@ const List = () => {
                 />
               </div>
               <div className={css.teacherInfo}>
-                <div className={css.titleWrapper}>
-                  <p className={css.titleText}>Languages</p>
-                  <h3
-                    className={css.title}
-                  >{`${teacher.name} ${teacher.surname}`}</h3>
+                <div className={css.teacherContainer}>
+                  <div className={css.titleWrapper}>
+                    <p className={css.titleText}>Languages</p>
+                    <h3
+                      className={css.title}
+                    >{`${teacher.name} ${teacher.surname}`}</h3>
+                  </div>
+                  <div className={css.infoWrapper}>
+                    <ul className={css.infoList}>
+                      <li className={css.infoListItem}>
+                        <svg width="16" height="16" className={css.infoIcon}>
+                          <use href="/sprite.svg#icon-book" />
+                        </svg>
+                        <p>Lessons online</p>
+                      </li>
+                      <li className={css.infoListItem}>
+                        Lessons done: {teacher.lessons_done}
+                      </li>
+                      <li className={css.infoListItem}>
+                        <svg
+                          className={css.infoIconStar}
+                          width="16"
+                          height="16"
+                        >
+                          <use href="/sprite.svg#icon-star" />
+                        </svg>
+                        Rating: {teacher.rating}
+                      </li>
+                      <li className={css.infoListItem}>
+                        Price / 1 hour:
+                        <span className={css.price}>
+                          {teacher.price_per_hour}$
+                        </span>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
-                <div className={css.infoWrapper}>
-                  <ul className={css.infoList}>
-                    <li className={css.infoListItem}>
-                      <svg width="16" height="16" className={css.infoIcon}>
-                        <use href="/sprite.svg#icon-book" />
-                      </svg>
-                      <p>Lessons online</p>
+                <div className={css.lessonDetailsWrapper}>
+                  <ul className={css.lessonDetailsList}>
+                    <li className={css.lessonDetailsItem}>
+                      Speaks:{" "}
+                      <span className={css.lessonDetailsSpan}>
+                        {teacher.languages.join(", ")}
+                      </span>
                     </li>
-                    <li className={css.infoListItem}>
-                      Lessons done: {teacher.lessons_done}
+                    <li className={css.lessonDetailsItem}>
+                      Lesson Info:{" "}
+                      <span className={css.lessonDetailsSpan}>
+                        {teacher.lesson_info}
+                      </span>
                     </li>
-                    <li className={css.infoListItem}>
-                      <svg className={css.infoIconStar} width="16" height="16">
-                        <use href="/sprite.svg#icon-star" />
-                      </svg>
-                      Rating: {teacher.rating}
-                    </li>
-                    <li className={css.infoListItem}>
-                      Price / 1 hour:
-                      <span className={css.price}>
-                        {teacher.price_per_hour}$
+                    <li className={css.lessonDetailsItem}>
+                      Conditions:{" "}
+                      <span className={css.lessonDetailsSpan}>
+                        {teacher.conditions}
                       </span>
                     </li>
                   </ul>
                 </div>
-                {/* <p>{teacher.lesson_info}</p> */}
-                {/* <p>{teacher.experience}</p> */}
-                {/* <p>{teacher.price_per_hour} €/h</p> */}
               </div>
             </li>
           ))}
