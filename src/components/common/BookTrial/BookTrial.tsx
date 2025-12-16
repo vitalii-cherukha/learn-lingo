192d24ce72729e46610459f8eaa01f18e161ec24
vitalii_cherukha <vitalii.cherukha@gmail.com>
2025-12-16 12:48:45 +0200

    add teacher information display and form submission logic in BookTrial component

--- src/components/common/BookTrial/BookTrial.tsx
+++ src/components/common/BookTrial/BookTrial.tsx
@@ -1,6 +1,9 @@
-import { Teacher } from "../../../types";
+import { useState } from "react";
+import { Teacher, User } from "../../../types";
 import Modal from "../Modal/Modal";
 import css from "./BookTrial.module.css";
+import { Form } from "react-router";
+import FormBookTrial from "../FormBookTrial/FormBookTrial";
 
 interface BookTrialProps {
   isOpen: boolean;
@@ -9,11 +12,24 @@ interface BookTrialProps {
 }
 
 const BookTrial = ({ isOpen, onClose, teacher }: BookTrialProps) => {
+  const [loading, setLoading] = useState(false);
   if (!isOpen) return null;
   if (!teacher) {
     console.log("Something went wrong");
     return null;
   }
+
+  const onSubmit = async (user: User) => {
+    setLoading(true);
+    try {
+      console.log(user);
+      onClose();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Modal onClose={onClose}>
@@ -24,6 +40,23 @@ const BookTrial = ({ isOpen, onClose, teacher }: BookTrialProps) => {
           your learning goals, and tailor the lesson to your specific needs.
         </p>
 
+        <div className={css.teacherWrapper}>
+          <div className={css.avatarWrapper}>
+            <img
+              src="/default-photo.jpg"
+              alt={teacher.name}
+              width="44"
+              height="44"
+            />
+          </div>
+          <div className={css.nameWrapper}>
+            <p className={css.label}>Your teacher</p>
+            <p className={css.name}>
+              {teacher.name} {teacher.surname}
+            </p>
+          </div>
+        </div>
+        <FormBookTrial loading={loading} onSubmit={onSubmit} />
         <button onClick={onClose}>
           <svg className={css.closeIcon} width="32" height="32">
             <use href="/sprite.svg#icon-close-modal" />
